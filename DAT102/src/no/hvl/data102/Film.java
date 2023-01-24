f39ff7797dd54fc257abe5581198ba5afa83c3f3
linnv <linnv@158.37.198.236>
2023-01-24 14:07:13 +0100

    test

---
+++ DAT102/src/no/hvl/data102/Film.java
@@ -0,0 +1,10 @@
+package no.hvl.data102;
+
+public class Film {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ DAT102/src/no/hvl/data102/Filmarkiv.java
@@ -0,0 +1,10 @@
+package no.hvl.data102;
+
+public class Filmarkiv {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
