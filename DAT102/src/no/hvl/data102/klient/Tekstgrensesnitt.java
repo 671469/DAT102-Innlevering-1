385a213bb6c962ee53a31e3a579f85bae4fe1c66
669773 <111857758+669773@users.noreply.github.com>
2023-02-01 17:31:23 +0100

    Update Tekstgrensesnitt.java

--- DAT102/src/no/hvl/data102/klient/Tekstgrensesnitt.java
+++ DAT102/src/no/hvl/data102/klient/Tekstgrensesnitt.java
@@ -2,9 +2,32 @@ package no.hvl.data102.klient;
 
 public class Tekstgrensesnitt {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	// lese opplysningene om en FILM fra tastatur
+	public Film lesFilm(){
+	// TODO
+	return null;
 	}
+	// vise en film med alle opplysninger på skjerm (husk tekst for sjanger)
+	public void visFilm(Film film) {
+	// TODO
+	}
+	// Skrive ut alle Filmer med en spesiell delstreng i tittelen
+	public void skrivUtFilmDelstrengITittel(FilmarkivADT filma,
+	 String delstreng) {
+	// TODO
+	}
+	// Skriver ut alle Filmer av en produsent / en gruppe
+	public void skrivUtFilmProdusent(FilmarkivADT filma, String delstreng) {
+	// TODO Denne gjør tilsvarende som metoden over
+	}
+	// Skrive ut en enkel statistikk som inneholder antall Filmer totalt
+	// og hvor mange det er i hver sjanger
+	public void skrivUtStatistikk(FilmarkivADT filma) {
+	// TODO
+	}
+	// … Ev. andre metoder
+
+}
+
 
 }
