488814f9fe95e171cc1df6a852b650d5dce8bb2f
669773 <111857758+669773@users.noreply.github.com>
2023-02-01 17:28:55 +0100

    Update klientFilmarkiv.java

--- DAT102/src/no/hvl/data102/klient/klientFilmarkiv.java
+++ DAT102/src/no/hvl/data102/klient/klientFilmarkiv.java
@@ -3,7 +3,9 @@ package no.hvl.data102.klient;
 public class klientFilmarkiv {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		FilmarkivADT filma = new Filmarkiv(100);
+		Meny meny = new Meny(filma);
+		meny.start();
 
 	}
 
