b1bd919fb1d8bd09f9c3e284846e63046fe97d7d
669773 <111857758+669773@users.noreply.github.com>
2023-02-01 17:25:32 +0100

    Update Film.java

--- DAT102/src/no/hvl/data102/Film.java
+++ DAT102/src/no/hvl/data102/Film.java
@@ -1,7 +1,80 @@
 package no.hvl.data102;
 
 public class Film {
+// 1.1 - Data
+	int filmNr;
+	String filmSkaper;
+	String tittel;
+	int lanseringsår;
+	//enum Sjanger; 
+	//Hvordan gjør man dette? Husk å sett inn i kontruktører og metoder!!
+	String filmSelskap;
 
+// Konstruktører
+	
+	Film tomFilm; // Tror dette kan være riktig, men vet egt ike hehe
+	
+	public Film(int filmNr, String filmSkaper, String tittel,
+			int lanseringsår, String filmSelskap) {
+		this.filmNr = filmNr;  this.tittel = tittel;
+		this.filmSkaper = filmSkaper;  this.lanseringsår = lanseringsår;
+		this.filmSelskap = filmSelskap;
+	}
+	  
+// Metoder
+	// get og set metoder
+	public void setFilmNr(int filmNr) {
+		this.filmNr = filmNr;
+	}
+	public void setFilmSkaper(String filmSkaper) {
+		this.filmSkaper = filmSkaper;
+	}
+	public void setTittel(String tittel) {
+		this.tittel = tittel;
+	}
+	public void setLanseringsår(int lanseringsår) {
+		this.lanseringsår = lanseringsår;
+	}
+	public void setFilmSelskap(String filmSelskap) {
+		this.filmSelskap = filmSelskap;
+	}
+	public int getFilmNr() {
+		return filmNr;
+	}
+	public String getFilmSkaper() {
+		return filmSkaper;
+	}
+	public String getTittel() {
+		return tittel;
+	}
+	public int getLanseringsår() {
+		return lanseringsår;
+	}
+	public String getFilmSelkap() {
+		return filmSelskap;
+	}
+	// equals metode
+	@Override
+	public boolean equals(Object obj) {
+		Film s = (Film) obj;
+		
+		if (getFilmNr() != s.getFilmNr()) {
+			return false;
+		}
+		else {
+			return true;
+		}
+	}
+	// hashcode metode
+	/* Oppgaven sier at om nr er det samme som medlemsvariabelen 
+	 * kan vi skrive hashcode slik som under, men hva er
+	 * medlemsvariablene? den er ikke nevnt noen steder tidligere!!
+	 */
+	@Override 
+	public int hashCode() { 
+	    Integer temp = nr; 
+	    return temp.hashCode(); 
+	} 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
