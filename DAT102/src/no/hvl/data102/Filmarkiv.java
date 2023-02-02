f3ecbc8eb0ded6a2c7d5cc682bde97562ff82d39
linnv <linnv@158.37.198.51>
2023-02-02 15:06:44 +0100

    filmarkiv

--- DAT102/src/no/hvl/data102/Filmarkiv.java
+++ DAT102/src/no/hvl/data102/Filmarkiv.java
@@ -1,10 +1,48 @@
 package no.hvl.data102;
 
-public class Filmarkiv {
+import no.hvl.data102.adt.FilmarkivADT;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+public class Filmarkiv implements FilmarkivADT{
+	Film[] filmTabell;
+	int antall;
+	
+	public Filmarkiv(int størrelse) {
+		filmTabell = new Film[størrelse];
+		antall = 0;
+	}
+	
+	@Override
+	public Film finnFilm(int nr) {
+		return null;
+	}
 
+	@Override
+	public void leggTilFilm(Film nyFilm) {
+	}
+
+	@Override
+	public boolean slettFilm(int filmnr) {
+		return false;
+	}
+
+	@Override
+	public Film[] soekTittel(String delstreng) {
+		return null;
+	}
+
+	@Override
+	public Film[] soekProdusent(String delstreng) {
+		return null;
+	}
+
+	@Override
+	public int antall(Sjanger sjanger) {
+		return 0;
+	}
+
+	@Override
+	public int antall() {
+		return 0;
 	}
 
 }
