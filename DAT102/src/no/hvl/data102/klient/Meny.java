a0a481458532241142ed34a9a0ac9e163c225655
astridibensnes <astridibensnes@158-37-232-136.hvl.no>
2023-02-03 18:09:19 +0100

    lagt inn noe på meny

--- DAT102/src/no/hvl/data102/klient/Meny.java
+++ DAT102/src/no/hvl/data102/klient/Meny.java
@@ -15,7 +15,23 @@ public class Meny {
 	}
 	
 	public void start(){
+		Film [] filmer;
+	    filmer = new Film[5];
+
+
+	    filmer[0] = new Film(1, "Christopher Nolan", "Interstellar",
+	                                 2014, Sjanger.SCIFI, "Legendary PP");
+
+	    filmer[1] = new Film(2, "Christopher Nolan", "Dunkirk",
+	                                 2017, Sjanger.ACTION, "Warner Bros");
+
+	    filmer[2] = new Film(3, "Megan Park", "The Fallout",
+	                                 2021, Sjanger.DRAMA, "New Line Cinema");
+
+			for (Film film : filmer) {
+	        this.filmarkiv.leggTilFilm(film);
 		
 	}
 
+}
 }
