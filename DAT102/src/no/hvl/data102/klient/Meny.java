50eacf524179e8b4494ba0ce00bdc0cf657013d3
astridibensnes <astridibensnes@158-37-232-136.hvl.no>
2023-02-03 17:02:46 +0100

    meny

--- DAT102/src/no/hvl/data102/klient/Meny.java
+++ DAT102/src/no/hvl/data102/klient/Meny.java
@@ -1,13 +1,17 @@
 package no.hvl.data102.klient;
 
+import no.hvl.data102.adt.FilmarkivADT;
+
 public class Meny {
 
 	private Tekstgrensesnitt tekstgr;
 	private FilmarkivADT filmarkiv;
+	
 	public Meny(FilmarkivADT filmarkiv){
-	tekstgr = new Tekstgrensesnitt();
-	this.filmarkiv = filmarkiv;
+		tekstgr = new Tekstgrensesnitt();
+		this.filmarkiv = filmarkiv;
 	}
+	
 	public void start(){
 	 // legg inn en del forhåndsdefinerte filmer for å teste metodene
 	// TODO
